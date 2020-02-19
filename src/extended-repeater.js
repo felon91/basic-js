6339e565122940248e674ebb487ec3bca57cd841
Denis Bulyha <denis.bulyha@infourok.biz>
2020-02-19 17:37:26 +0300

    feat: implement function extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    options.separator = options.separator || '+';
+    options.addition = String(options.addition) || '';
+    options.additionSeparator = options.additionSeparator || '|';
+    let result = '';
+    let addResult = '';
+
+    if (options.additionRepeatTimes) {
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+            addResult += (options.additionRepeatTimes - 1 === j) ? `${options.addition}` : `${options.addition}${options.additionSeparator}`;
+        }
+    }
+
+    if (options.repeatTimes) {
+        for (let i = 0; i < options.repeatTimes; i++) {
+            result += (options.repeatTimes - 1 === i) ? `${str}${addResult}` : `${str}${addResult}${options.separator}`;
+        }
+    } else {
+        result = `${str}${options.addition}`;
+    }
+    return result;
 };
-  
